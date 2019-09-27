7da46d72f2fab5777efd125d94bdeee77eca1117
rooted-cyber <50019150+rooted-cyber@users.noreply.github.com>
2019-09-27 14:15:58 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,9 +6,10 @@ apt-get install toilet
 apt-get install figlet
 c="toilet -f term -F gay"
 cp -f fixed $PREFIX/bin
-chmod 777 $PREFIX/bin/fix
+chmod 777 $PREFIX/bin/fixed
 mv Fix-Green $PREFIX
 chmod -R 777 $PREFIX/Fix-Green
 clear
 echo "You Can Start This Command :-"|$c
+echo
 echo "		fixed"
